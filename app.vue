0af0ad1e83e5d265ce246567cdac9da020f05720
Vinnie Marone <vinnie.marone35@gmail.com>
2024-07-07 21:31:49 -0400

    add vuetify and nuxt page

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-container>
+        <NuxtPage />
+        <v-footer>
+          <v-row justify="center" no-gutters>
+            <v-col class="text-center mt-4" cols="12">
+              {{ new Date().getFullYear() }} — <strong>Teaburys</strong>
+            </v-col>
+          </v-row>
+        </v-footer>
+      </v-container>
+    </v-app>
+  </NuxtLayout>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-main>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" class="d-flex align-center">
+          <div class="mx-auto text-center">
+            <h3 class="banner-title font-weight-bold text-white">
+              welcome to Teaburys
+            </h3>
+            <h1>The Barber that Cares</h1>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
