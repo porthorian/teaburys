a7a27054521d210dc1ce5de988193ea2ace680e2
Vinnie Marone <vinnie.marone35@gmail.com>
2024-10-18 22:24:50 -0400

    add more links and instagram link

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,12 @@
           <v-col class="text-center mt-auto" cols="12">
               {{ new Date().getFullYear() }} — <strong>Teaburys</strong>
           </v-col>
+          <v-col class="text-center mt-auto" cols="12">
+              <v-btn icon="mdi-instagram" href="https://www.instagram.com/ro_cuts?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="></v-btn>
+              <v-btn class="rounded-circle" icon="" href="https://www.vagaro.com/theshopatteaburyllc">
+                <v-img height="24" width="24" src="/vegaro.png"></v-img>
+              </v-btn>
+          </v-col>
         </v-row>
       </v-footer>
     </v-container>
--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,25 @@
               aspect-ratio="1/1" cover src="/the-shop.png"
                max-height="450" max-width="450"
                class="mx-auto"></v-img>
-            <v-btn href="https://www.vagaro.com/theshopatteaburyllc/book-now">Book Now</v-btn>
+            <v-row class="mt-2">
+              <v-btn class="mx-auto" href="https://www.vagaro.com/theshopatteaburyllc/book-now">Book Now</v-btn>
+            </v-row>
+            <v-row class="mt-5">
+              <v-btn
+                class="mx-auto"
+                prepend-icon="mdi-instagram"
+                href="https://www.instagram.com/ro_cuts?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+              >@ro_cuts</v-btn>
+            </v-row>
           </div>
         </v-col>
       </v-row>
     </v-container>
   </v-main>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const dialog = ref(false)
+</script>
